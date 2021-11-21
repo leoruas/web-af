7f41e989cbc48cd2103c4a4d349ce876a10595a8
leoruas <leo.coelho.ruas@gmail.com>
2021-11-21 10:40:39 -0300

    added app bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <v-app>
-    <v-system-bar dark color="primary">
+    <v-system-bar dark app color="grey darken-3">
       <v-spacer></v-spacer>
       <v-icon>mdi-wifi-strength-4</v-icon>
       <v-icon>mdi-signal-cellular-outline</v-icon>
       <v-icon>mdi-battery</v-icon>
       <span>12:30</span>
     </v-system-bar>
+
+    <v-app-bar color="grey darken-4" dark app absolute clipped-left>
+      <v-icon left>mdi-earth</v-icon>
+      <v-toolbar-title>Planetarium</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-alien</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
   </v-app>
 </template>
 
