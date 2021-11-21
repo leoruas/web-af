f1a1b82ecd6eba03df5d0f8ffa37566b57c7e9dc
leoruas <leo.coelho.ruas@gmail.com>
2021-11-21 11:29:40 -0300

    added content

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,10 @@
     </v-app-bar>
 
     <!-- Navigation Drawer -->
-    <v-navigation-drawer app permanent absolute clipped color="grey lighten-3">
+    <v-navigation-drawer dark app permanent absolute clipped>
       <v-sheet class="pa-1">
         <v-list-item>
-          <v-list-item-avatar color="grey darken-3" size="64">
+          <v-list-item-avatar color="grey darken-2" size="64">
             <v-img src="./assets/Elgyem.png"></v-img>
           </v-list-item-avatar>
 
@@ -44,6 +44,7 @@
       <v-divider></v-divider>
 
       <v-list>
+        <v-subheader>PLANETS</v-subheader>
         <v-list-item-group v-model="selectedItem" color="">
           <v-list-item v-for="item in navbarItems" :key="item.title" link>
             <v-list-item-icon>
@@ -58,6 +59,40 @@
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
+
+    <!-- Content -->
+    <v-main class="pa-0">
+      <v-content style="background-color: #2b2b2b; height: 100%">
+        <v-row style="color: white" class="pa-4" justify="space-around">
+          <v-col cols="7" style="text-align: justify">
+            <h1>Uranus</h1>
+            <v-divider class="background-color: white mb-3"></v-divider>
+            Uranus is the seventh planet from the Sun. Its name is a reference
+            to the Greek god of the sky, Uranus, who, according to Greek
+            mythology, was the great-grandfather of Ares (Mars), grandfather of
+            Zeus (Jupiter) and father of Cronus (Saturn). It has the
+            third-largest planetary radius and fourth-largest planetary mass in
+            the Solar System. Uranus is similar in composition to Neptune, and
+            both have bulk chemical compositions which differ from that of the
+            larger gas giants Jupiter and Saturn. For this reason, scientists
+            often classify Uranus and Neptune as "ice giants" to distinguish
+            them from the other giant planets. Uranus's atmosphere is similar to
+            Jupiter's and Saturn's in its primary composition of hydrogen and
+            helium, but it contains more "ices" such as water, ammonia, and
+            methane, along with traces of other hydrocarbons. It has the
+            coldest planetary atmosphere in the Solar System, with a minimum
+            temperature of 49 K (−224 °C; −371 °F), and has a complex, layered
+            cloud structure with water thought to make up the lowest clouds and
+            methane the uppermost layer of clouds. The interior of Uranus is
+            mainly composed of ices and rock.
+          </v-col>
+
+          <v-col cols="4">
+            <v-img src="./assets/Uranus.png"></v-img>
+          </v-col>
+        </v-row>
+      </v-content>
+    </v-main>
   </v-app>
 </template>
 
