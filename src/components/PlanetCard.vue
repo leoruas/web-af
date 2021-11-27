8cdc0763ae0834bb3e2aaffd134866793811bf75
leoruas <leo.coelho.ruas@gmail.com>
2021-11-26 21:13:08 -0300

    added state planets array and implemented planets list in home

---
+++ src/components/PlanetCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card dark>
+    <v-img
+      height="300px"
+      contain
+      :src="require(`@/assets/${planet.image}`)"
+    ></v-img>
+    <v-card-title> {{ planet.name }} </v-card-title>
+    <v-card-text> {{ planet.description.substring(0, 100) }}... </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: ["planet"]
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,31 @@
 <template>
   <v-content style="background-color: #303030; height: 100%">
     <v-row style="color: white" class="pa-4">
-      <v-col cols="8" style="text-align: justify">
+      <v-col cols="12" style="text-align: justify">
         <h1>Home</h1>
         <v-divider class="background-color: white mb-3"></v-divider>
+
+        <v-row>
+          <v-col v-for="(planet, p) in planets" :key="p" cols="3">
+            <PlanetCard :planet="planet" />
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-content>
 </template>
+
+<script>
+import PlanetCard from "@/components/PlanetCard.vue";
+
+export default {
+  components: {
+    PlanetCard
+  },
+  computed: {
+    planets() {
+      return this.$store.state.planets;
+    },
+  },
+};
+</script>
