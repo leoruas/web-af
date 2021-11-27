d0a732642a51ce8deaf9aae2bdae8da24828b7c7
leoruas <leo.coelho.ruas@gmail.com>
2021-11-26 21:18:34 -0300

    populated recents page with sotre info

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,6 +1,6 @@
 <template>
   <v-content style="background-color: #303030; height: 100%">
-    <v-row style="color: white" class="pa-4" >
+    <v-row style="color: white" class="pa-4">
       <v-col cols="12" style="text-align: justify">
         <h1>Favorites</h1>
         <v-divider class="background-color: white mb-3"></v-divider>
@@ -20,15 +20,15 @@ import PlanetCard from "@/components/PlanetCard.vue";
 
 export default {
   components: {
-    PlanetCard
+    PlanetCard,
   },
   computed: {
     planets() {
       return this.$store.state.planets;
     },
     favorites() {
-      return this.planets.filter(planet => planet.isFavorite)
-    }
+      return this.planets.filter((planet) => planet.isFavorite);
+    },
   },
 };
 </script>
